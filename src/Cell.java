4895cad7fd8a2828d71a2399da87b0c12dca2fc3
Nicholas Bowers <greennick7777@gmail.com>
2021-03-14 21:48:51 -0400

    Created basic Cell class

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,43 @@
 public class Cell {
+    private boolean isAlive;
+
+    /**
+     * Default constructor that creates a new dead cell.
+     *
+     */
+    public Cell() {
+        this(false);
+    }
+
+    /**
+     * Constructor that creates a new Cell with a specified living status.
+     *
+     * @param isAlive true if the new cell is alive, false if it is dead.
+     */
+    public Cell(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    /**
+     * Accessor method that returns the current living status of the cell.
+     *
+     * @return true if the cell is alive, false if it is dead.
+     */
+    public boolean getIsAlive() {
+        return isAlive;
+    }
+
+    /**
+     * Mutator method that updates the cell to be alive or dead.
+     *
+     * @param isAlive true to bring the cell to life, false to kill it.
+     */
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    @Override
+    public String toString() {
+        return (isAlive) ? "▓" : "░";
+    }
 }
