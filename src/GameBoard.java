ebd44cdfd297d7217f6e7ee2ee294ee4354d0b26
Nicholas Bowers <greennick7777@gmail.com>
2021-03-25 02:22:01 -0400

    Added updateBoardState method

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,10 +1,10 @@
 import java.util.Random;
 
 public class GameBoard {
-    private int xSize;
-    private int ySize;
+    private final int xSize;
+    private final int ySize;
+    private final Random rand;
     private Cell[][] backingArray;
-    private Random rand;
 
     /**
      * Default constructor that creates a new 500 by 500 GameBoard.
@@ -54,9 +54,87 @@ public class GameBoard {
     public void randomize() {
         for (Cell[] row : backingArray) {
             for (Cell element : row) {
-                element.setIsAlive(rand.nextInt(2) == 0);
+                element.setStatus(rand.nextInt(2) == 0);
             }
         }
+    }
+
+    /**
+     * Replaces the current backingArray with a new backingArray containing
+     * updated cells corresponding to the GameBoard's next state. Cells are
+     * updated according to the rules of Conway's Game of Life. The GameBoard is
+     * a torus. Cells at the top of the board neighbor cells at the bottom, and
+     * cells at the right of the board neighbor cells at the left.
+     *
+     * Rules:
+     * 1. Any live cell with fewer than two live neighbors dies.
+     * 2. Any live cell with more than three live neighbors dies.
+     * 3. Any live cell with two or three live neighbors lives.
+     * 4. Any dead cell with exactly three live neighbors will come to life.
+     *
+     */
+    public void updateBoardState() {
+        Cell[][] nextBoard = new Cell[xSize][ySize];
+
+        for (int x = 0; x < xSize; x++) {
+            for (int y = 0; y < ySize; y++) {
+                int numNeighbors = 0;
+
+                // Check top
+                numNeighbors += checkCell(x, (y + 1) % ySize);
+                // Check top-right
+                numNeighbors += checkCell((x + 1) % xSize, (y + 1) % ySize);
+                // Check right
+                numNeighbors += checkCell((x + 1) % xSize, y);
+                // Check bottom-right
+                numNeighbors += checkCell((x + 1) % xSize, (y - 1) % ySize);
+                // Check bottom
+                numNeighbors += checkCell(x, (y - 1) % ySize);
+                // Check bottom-left
+                numNeighbors += checkCell((x - 1) % xSize, (y - 1) % ySize);
+                // Check left
+                numNeighbors += checkCell((x - 1) % xSize, (y) % ySize);
+                // Check top-left
+                numNeighbors += checkCell((x - 1) % xSize, (y + 1) % ySize);
+
+                if (backingArray[x][y].getStatus() && (numNeighbors < 2)) {
+                    nextBoard[x][y] = new Cell(false);
+                } else if (backingArray[x][y].getStatus()
+                        && (numNeighbors > 3)) {
+                    nextBoard[x][y] = new Cell(false);
+                } else if (backingArray[x][y].getStatus()) {
+                    nextBoard[x][y] = new Cell(true);
+                } else if (!backingArray[x][y].getStatus()
+                        && (numNeighbors == 3)) {
+                    nextBoard[x][y] = new Cell(true);
+                } else {
+                    nextBoard[x][y] = new Cell(false);
+                }
+            }
+        }
+
+        backingArray = nextBoard;
+    }
+
+    /**
+     * Helper method that checks if the specified cell in the backingArray is
+     * alive or not and returns 1 or 0. Used to help calculate the number of
+     * living neighbors around a cell.
+     *
+     * @param x x coordinate of the cell
+     * @param y y coordinate of the cell
+     * @return 1 if the cell is alive, 0 if it is dead
+     */
+    private int checkCell(int x, int y) {
+        if (x == -1) {
+            x = xSize - 1;
+        }
+
+        if (y == -1) {
+            y = ySize - 1;
+        }
+
+        return (backingArray[x][y].getStatus()) ? 1 : 0;
     }
 
     /**
