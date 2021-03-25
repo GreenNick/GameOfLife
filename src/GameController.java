551edee926c8645af02d447e73e66e18f93e19ba
Nicholas Bowers <greennick7777@gmail.com>
2021-03-25 02:49:12 -0400

    Added basic JavaFX setup

--- src/GameController.java
+++ src/GameController.java
@@ -1,2 +1,32 @@
-public class GameController {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+
+public class GameController extends Application {
+    private GameBoard board;
+    private Stage stage;
+
+    /**
+     * Main method that launches the game.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+        Canvas canvas = new Canvas();
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+
+        Scene scene = new Scene(root, board.getXSize(), board.getYSize());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
