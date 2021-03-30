5306a1aaaf12eb17f02d2a0dc0f928c95b7ef10b
Nicholas Bowers <greennick7777@gmail.com>
2021-03-29 22:35:39 -0400

    Fixed JavaFX setup and added updateCanvas() method

--- src/GameController.java
+++ src/GameController.java
@@ -3,12 +3,24 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class GameController extends Application {
     private final int canvasWidth;
     private final int canvasHeight;
     private final GameBoard board;
+    private final Canvas canvas;
+    private final GraphicsContext graphicsContext;
+
+    /**
+     * Default Constructor that creates a new GameController with a backing
+     * board that has 75 columns and 50 rows.
+     *
+     */
+    public GameController() {
+        this(75, 50);
+    }
 
     /**
      * Constructor that creates a new GameController with a GameBoard of a
@@ -21,6 +33,9 @@ public class GameController extends Application {
         canvasWidth = xSize * 10;
         canvasHeight = ySize * 10;
         board = new GameBoard(xSize, ySize);
+        board.randomize();
+        canvas = new Canvas(canvasWidth, canvasHeight);
+        graphicsContext = canvas.getGraphicsContext2D();
     }
 
     /**
@@ -29,18 +44,36 @@ public class GameController extends Application {
      * @param args command line arguments
      */
     public static void main(String[] args) {
-        GameController gameController = new GameController(100, 100);
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) {
         Group root = new Group();
-        Canvas canvas = new Canvas(canvasWidth, canvasHeight);
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
         root.getChildren().add(canvas);
+        updateCanvas();
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    /**
+     * Private helper method that re-renders the canvas to reflect the current
+     * state of the backing array.
+     *
+     */
+    private void updateCanvas() {
+        for (int x = 0; x < board.getXSize(); x++) {
+            for (int y = 0; y < board.getYSize(); y++) {
+                if (board.getBackingArray()[x][y].getStatus()) {
+                    graphicsContext.setFill(Color.BLACK);
+                } else {
+                    graphicsContext.setFill(Color.WHITE);
+                }
+
+                graphicsContext.fillRect(x * 10, y * 10, 10, 10);
+            }
+        }
     }
 }
